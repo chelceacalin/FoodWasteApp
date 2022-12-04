04f1d930720292a6235beeb41188c810e8aa87f3
MarioBujor2001 <bujormario2001@gmail.com>
2022-12-04 20:24:51 +0200

    second commit

--- back_end/server.js
+++ back_end/server.js
@@ -1,9 +1,34 @@
 import express from 'express';
+import { Sequelize, Model, DataTypes } from 'sequelize';
 const app = express();
 const port = 3000;
+const sequelize = new Sequelize(
+    'fortw',
+    'forTW',
+    '1234abc!',
+    {
+        host: 'localhost',
+        dialect: 'mysql'
+    }
+);
+
+const User = sequelize.define('Users', {
+    username: DataTypes.STRING,
+    name: DataTypes.DATE
+})
+
+sequelize.sync().then(async function () {
+    console.log('Everything is synced')
+});
+
+sequelize.authenticate().then(() => {
+    console.log('Conn established');
+}).catch((err) => {
+    console.log('Unable to connect to database:', err);
+})
 
 app.get('/api', (req, res) => {
-    res.send('Hello2!');
+    res.send('Created');
 })
 
 app.listen(port, () => {
