09e3bff4f56ae2339034c8069ad3cbf6796b46de
MarioBujor2001 <bujormario2001@gmail.com>
2022-12-29 15:04:56 +0200

    Updated interface looks for card item

--- proiect_tw/src/components/main_page/FoodCard.jsx
+++ proiect_tw/src/components/main_page/FoodCard.jsx
@@ -14,11 +14,11 @@ const FoodCard = () => {
             </div>
             <img src="https://upload.wikimedia.org/wikipedia/commons/6/6d/Good_Food_Display_-_NCI_Visuals_Online.jpg" />
             <p>Titlu Mancare</p>
-            <p>Donator: User1234
-                Cantitate: 2 u.m: kg
-                Expiration date: 20/10/22
-                Locatie: Bucuresti Sector 6
-                Telefon: 0766778899</p>
+            <p className="textContent">Donator: User1234</p>
+            <p className="textContent">Cantitate: 2 u.m: kg</p>
+            <p className="textContent">Expiration date: 20/10/22</p>
+            <p className="textContent">Locatie: Bucuresti Sector 6</p>
+            <p className="textContent">Telefon: 0766778899</p>
             <button>Rezerva</button>
         </div>
     )
