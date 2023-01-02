7210ae3d6cecb1dfc937869abe2ef29c5b768953
MarioBujor2001 <bujormario2001@gmail.com>
2023-01-02 17:28:16 +0200

    Added functionality to change profile pic, and upload to firebase

--- proiect_tw/src/components/main_page/LeftMenu.jsx
+++ proiect_tw/src/components/main_page/LeftMenu.jsx
@@ -3,32 +3,57 @@ import { onAuthStateChanged, signOut } from "firebase/auth";
 import auth from '../../firebase.js'
 import { useNavigate } from 'react-router-dom';
 import "./LeftMenu.css"
-import axios from "axios";
+import axios, { all } from "axios";
+import storage from '../../firebaseStorage.js'
+import { ref, uploadBytes, listAll, getDownloadURL } from 'firebase/storage';
+import { v4 } from 'uuid'
 const LeftMenu = (props) => {
     const navigate = useNavigate();
     //console.log("Ce"+auth.currentUser.uid);
-    let [numeUtilizator, setNumeUtilizator] = useState('')
-
+    let [numeUtilizator, setNumeUtilizator] = useState('');
+    let [profilePic, setProfilePic] = useState(null);
+    let [currentProfilePic, setCurrentProfilePic] = useState('');
+    let [dataForUpdate, setDateForUpdate] = useState({});
     const getNumeUser = async () => {
         const dataFromAxios = await axios.get('http://localhost:3030/api/users/' + auth.currentUser.uid);
         //console.log(dataFromAxios.data.username.split(" ")[0]);
         setNumeUtilizator(dataFromAxios.data.username.split(" ")[0]);
+        setDateForUpdate(dataFromAxios.data);
+        setCurrentProfilePic(dataFromAxios.data.photoUrl);
     }
 
     useEffect(() => {
         getNumeUser();
     }, [])
 
-    //console.log(numeUtilizator);
 
-    const editProfile=()=>{
-        navigate("../editProfile", { replace: true,state: auth.currentUser.uid});
+    //console.log(numeUtilizator);
+    const uploadImage = () => {
+        if (profilePic == null) {
+            return;
+        }
+        const imgRef = ref(storage, `ProfilePics/${v4() + '_' + profilePic.name}`);
+        uploadBytes(imgRef, profilePic).then(() => {
+            getDownloadURL(imgRef).then((res) => {
+                setCurrentProfilePic(res);
+                console.log(res);
+                let copieDateUtilizator = JSON.parse(JSON.stringify(dataForUpdate));
+                copieDateUtilizator.photoUrl = res;
+                console.log(copieDateUtilizator, res);
+                const linkUpdate = "http://localhost:3030/api/users/" + copieDateUtilizator.id
+                axios.put(linkUpdate, copieDateUtilizator).catch(err => { console.log(err) });
+            })
+        })
+    };
+    const editProfile = () => {
+        navigate("../editProfile", { replace: true, state: auth.currentUser.uid });
     }
 
     return (
         <div className="leftMenu">
             <div className="leftMenuProfile">
-                <img src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" />
+                <img src={currentProfilePic.length > 1 ? currentProfilePic : 'https://firebasestorage.googleapis.com/v0/b/proiecttw-84ef3.appspot.com/o/ProfilePics%2F1244141-200.png?alt=media&token=88ffe4ee-cb6a-4ab7-aad9-73da30ea92c2'} onClick={uploadImage} title="Click to upload image" />
+                <input type="file" onChange={(event) => { setProfilePic(event.target.files[0]) }} />
                 <p>Welcome,<span style={{ color: '#e3a1ff' }}> {numeUtilizator} </span> </p>
                 <button className="button-35" onClick={editProfile}>Edit profile</button>
                 <button className="button-35" onClick={props.addProduct}>Add product</button>
--- proiect_tw/src/firebaseStorage.js
+++ proiect_tw/src/firebaseStorage.js
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,6 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
 const storage = getStorage(app);
-export default auth;
+export default storage;
